ea77143d402627b7d122f817e38a1e82be8724ee
Jonathan Lemos <jonathanlemos@protonmail.com>
2019-03-21 21:09:30 -0400

    converted functions to class

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from functools import reduce
-from typing import Callable, Dict, List, Set, Tuple, TypeVar
+from typing import Callable, Dict, List, Sequence, Set, Tuple, TypeVar
 
 
 T = TypeVar("T")
-
-# first is the set of rules (str -> Set[Tuple[str]])
-# second is the start symbol
-cfg_type = Tuple[Dict[str, Set[Tuple[str]]], str]
 
 
 def first(fn: Callable[[T], bool], c: List[T]) -> int:
@@ -16,107 +12,105 @@ def first(fn: Callable[[T], bool], c: List[T]) -> int:
     return -1
 
 
-def cfg_tostring(ip: cfg_type) -> str:
-    x = ip[0][ip[1]]
-    y = [(a, b) for a, b in ip[0].items() if a != ip[1]]
+class Ruleset:
+    rules: Dict[str, Set[Tuple[str]]] = {}
+    start: str = ""
 
-    def f(a, c): return a + " | " + reduce(lambda b, e: b + " " + e, c).replace("#", "Ɛ")
+    def __init__(self, cfg: Sequence[str] = ()):
+        for rule in cfg:
+            # split name of rule and its right hand side
+            a = [x.strip() for x in rule.split("->")]
+            if len(a) < 2:
+                raise Exception("\"->\" not found in rule \"" + rule + "\"")
+            if len(a) > 2:
+                raise Exception("Multiple \"->\" found in rule \"" + rule + "\"")
 
-    ret = ip[1] + " -> " + reduce(f, x, "")[2:]
-    for d in y:
-        ret += "\n" + d[0] + " -> " + reduce(f, d[1], "")[2:]
-    return ret
+            # split by "|"
+            b = [x.strip() for x in a[1].split("|")]
+            if len(b) == 0:
+                raise Exception("Cannot have empty right hand in CFG")
 
+            if not a[0] in self.rules:
+                self.rules[a[0]] = set()
 
-# Input for the cfg must be as follows:
-# S -> 0 S 0 | 1 S 1 | A | #
-# pound sign = epsilon
-# any distinct token must be seperated by whitespace
-def parse_cfg(ip: List[str]) -> cfg_type:
-    ret: Dict[str, Set[Tuple[str]]] = {}
-    for s in ip:
-        # split name of rule and its right hand side
-        a = [x.strip() for x in s.split("->")]
-        if len(a) < 2:
-            raise Exception("\"->\" not found in rule \"" + s + "\"")
-        if len(a) > 2:
-            raise Exception("Multiple \"->\" found in rule \"" + s + "\"")
+            # foreach rule in b,
+            for r in b:
+                # get rid of epsilons unless epsilon is the only member of that rule
+                c = tuple(x for x in r.split() if len(r.split()) == 1 or x != "#")
+                if len(c) == 0:
+                    raise Exception("Cannot have empty rules in CFG rule \"" + r + "\"")
+                # add it to the list
+                self.rules[a[0]].add(c)
 
-        # split by "|"
-        b = [x.strip() for x in a[1].split("|")]
-        if len(b) == 0:
-            raise Exception("Cannot have empty right hand in CFG")
+        self.start = cfg[0].split("->")[0].strip() if len(cfg) > 0 else ""
 
-        if not a[0] in ret:
-            ret[a[0]] = set()
-
-        # foreach rule in b,
-        for rule in b:
-            # get rid of epsilons unless epsilon is the only member of that rule
-            c = tuple(x for x in rule.split() if len(rule.split()) == 1 or x != "#")
-            if len(c) == 0:
-                raise Exception("Cannot have empty rules in CFG rule \"" + s + "\"")
-            # add it to the list
-            ret[a[0]].add(c)
-
-    return ret, ip[0].split("->")[0].strip()
-
-
-# removes indirect left recursion
-def remove_ilr(ip: cfg_type) -> cfg_type:
-    productions: Dict[str, Set[Tuple[str]]] = ip[0]
-    ret: Dict[str, Set[Tuple[str]]] = {}
-    for nt1 in productions:
-        nt1: str = nt1
-        for nt2 in productions:
-            nt2: str = nt2
-            new: Set[Tuple[str]] = set()
-            for rule in productions[nt1]:
-                rule: Tuple[str] = rule
-                index = first(lambda x: x != nt1, list(rule))
-                if index == -1:
-                    raise Exception("Cannot have production of only itself in a CFG (\"" + nt1 + "\"")
-                if index > 0:
-                    for rhs in productions[nt2]:
-                        new.add(tuple(list(rhs) + list(rule[index:])))
+    def remove_recursion(self):
+        ret: Dict[str, Set[Tuple[str]]] = {}
+        for nt1 in list(self.rules):
+            nt1: str = nt1
+            for nt2 in list(self.rules):
+                nt2: str = nt2
+                new: Set[Tuple[str]] = set()
+                for rule in self.rules[nt1]:
+                    rule: Tuple[str] = rule
+                    index = first(lambda x: x != nt1, list(rule))
+                    if index == -1:
+                        raise Exception("Cannot have production of only itself in a CFG (\"" + nt1 + "\"")
+                    if index > 0:
+                        for rhs in self.rules[nt2]:
+                            new.add(tuple(list(rhs) + list(rule[index:])))
+                    else:
+                        new.add(rule)
+                if nt1 not in ret:
+                    ret[nt1] = new
                 else:
-                    new.add(rule)
-            if nt1 not in ret:
-                ret[nt1] = new
-            else:
-                for e in new:
-                    ret[nt1].add(e)
-    return ret, ip[1]
+                    for e in new:
+                        ret[nt1].add(e)
+                self.rules[nt1] = new
+                self.remove_dlr(nt1)
 
+    def remove_dlr(self, rule: str):
+        new_rule = set()
 
-# remove direct left recursion
-def remove_dlr(ip: cfg_type) -> cfg_type:
-    productions: Dict[str, Set[Tuple[str]]] = ip[0]
-    ret: Dict[str, Set[Tuple[str]]] = {}
-    for nt in productions:
-        nt: str = nt
-        ret[nt] = set()
+        def f(y): return y != rule
 
-        def f(y): return y != nt
+        lrec: List[Tuple[str, Tuple[str]]] = [(x[:first(f, list(x))], x[first(f, list(x)):]) for x in self.rules[rule] if first(f, list(x)) != -1 and first(f, list(x)) > 0]
+        notlrec: List[Tuple[str]] = [x for x in self.rules[rule] if first(f, list(x)) == 0]
+        ruleprime = rule + "'"
 
-        lrec: List[Tuple[str, Tuple[str]]] = [(x[:first(f, list(x))], x[first(f, list(x)):]) for x in productions[nt] if first(f, list(x)) != -1 and first(f, list(x)) > 0]
-        notlrec: List[Tuple[str]] = [x for x in productions[nt] if first(f, list(x)) == 0]
-        ntp = nt + "'"
+        if len(lrec) == 0:
+            return
 
         for n in notlrec:
-            ret[nt].add(tuple(list(n) + [ntp]))
+            new_rule.add(tuple(list(n) + [ruleprime]))
 
-        ret[ntp] = {("#",)}
+        new_rule_prime = {("#",)}
 
         for a, b in lrec:
-            ret[ntp].add(tuple(list(b) + [a]))
-    return ret, ip[1]
+            new_rule_prime.add(tuple(list(b) + list(a)))
+
+        self.rules[rule] = new_rule
+        self.rules[ruleprime] = new_rule_prime
+
+    def __len__(self):
+        return len(self.rules)
+
+    def __str__(self):
+        x = self.rules[self.start]
+        y = [(a, b) for a, b in self.rules.items() if a != self.start]
+
+        def f(a, c): return a + " | " + reduce(lambda b, e: b + " " + e, c).replace("#", "Ɛ")
+
+        ret = self.start + " -> " + reduce(f, x, "")[2:]
+        for d in y:
+            ret += "\n" + d[0] + " -> " + reduce(f, d[1], "")[2:]
+        return ret
 
 
 def main():
-    x = parse_cfg(["S -> S A | B", "A -> a", "B -> b"])
-    y = remove_dlr(x)
-    print(cfg_tostring(x))
+    x = Ruleset(["S -> Ab", "A -> # | Sc"])
+    x.remove_recursion()
+    print(str(x))
 
 
 if __name__ == '__main__':
