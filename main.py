9cbf4dbe8110584c84dc58015696d25e04b36377
Jonathan Lemos <jonathanlemos@protonmail.com>
2019-04-27 19:18:05 -0400

    yeet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from functools import reduce
 import itertools
+import re
 from typing import Callable, Dict, Iterable, Iterator, List, Pattern, Sequence, Set, Tuple, TypeVar
 
 
@@ -124,7 +125,6 @@ def tokenize(rhs: str) -> Tuple[str]:
     else:
         return tuple(ret)
 
-
 class Nonterm:
     __symbol: str = ""
     __productions: Set[Tuple[str]]
@@ -145,10 +145,7 @@ class Nonterm:
         return iter(self.__productions)
 
     def __str__(self) -> str:
-        def p2s(prod: Tuple[str]) -> str:
-            return reduce(lambda a, b: a + " " + b, prod)
-
-        return self.__symbol + " -> " + reduce(lambda a, b: a + " | " + p2s(b), self.__productions, "")[3:]
+        return self.__symbol + " -> " + reduce(lambda a, b: a + " | " + " ".join(b), self.__productions, "")[3:]
 
 
 class Grammar:
@@ -221,107 +218,220 @@ class Grammar:
     def terminals(self) -> Iterable[str]:
         return self.__terminals
 
+    """
+    Returns all of the non-terminals that can produce epsilon.
+    """
     def epsilon_nonterms(self) -> Set[str]:
         ret = {"#"}
 
+        # while the set is changing
         while True:
             len_tmp = len(ret)
             for nt, prod in self:
                 hit = False
                 for token in prod:
-                    if token not in prod:
+                    # if the token is not in our current set of epsilons, stop
+                    if token not in ret:
                         hit = True
                         break
+                # if all the tokens could produce epsilon, add it to our set
                 if not hit:
                     ret |= {nt}
+            # if the set didn't change on this iteration, break
             if len_tmp == len(ret):
                 return ret
 
+    """
+    Returns all of the first sets in the grammar.
+    
+    :returns: A dictionary mapping each non-terminal to a set of terminals
+    """
     def first_sets(self) -> Dict[str, Set[str]]:
         ret = {}
         for nt in self.nonterms():
             ret[nt] = set()
+        # treat terminals as having themselves in the first set.
         for t in self.terminals():
             ret[t] = {t}
         epsilons = self.epsilon_nonterms()
 
+        # while the grammar is changing.
+        # a non recursive loop prevents this from choking on left recursive grammars
         while True:
             updated = False
             for nt, prod in self:
+                # go along this production until we hit a token that can't produce epsilon
                 for token in prod:
                     tmp = len(ret[nt])
+
+                    # the current nt should include the first set of this token
                     ret[nt] |= ret[token]
+
+                    # if the length of this set changed, set updated to true
                     updated |= len(ret[nt]) != tmp
+
+                    # stop if this token cannot produce epsilon
                     if token not in epsilons:
                         break
+            # when there are no longer any changes to make
             if not updated:
-                return ret
+                # return the dictionary minus the terminals
+                return {x: ret[x] for x in ret if x not in self.terminals()}
 
+    """
+    Returns all of the follow sets in the grammar.
+    
+    :returns: A dictionary mapping each non-terminal to a set of terminals
+    """
     def follow_sets(self) -> Dict[str, Set[str]]:
+        # get first sets, add our terminals back in
         fs = self.first_sets()
+        fs.update({x: {x} for x in self.terminals()})
+
         epsilons = self.epsilon_nonterms()
+
         ret = {}
         for nt in self.nonterms():
             ret[nt] = set()
+        # start symbol can always be followed by dollar sign
+        ret[self.start()] = {"$"}
 
+        # while the set is changing
         while True:
             updated = False
 
-            def update(s1: Set[str], s2: Set[str]) -> None:
-                global updated
-                tmp = len(s1)
+            def update(s1: Set[str], s2: Set[str]) -> bool:
+                mpt = len(s1)
                 s1 |= s2
-                updated = len(s1) == tmp
+                return len(s1) != mpt
 
             for nt, prod in self:
+                # tmp is what can follow the current symbol
+                tmp = ret[nt]
                 for token in reversed(prod):
-                    if token in ret:
-                        update(ret[token], ret[nt])
-                        if token in epsilons:
-                            update(ret[nt], ret[token])
-                        else:
-                            update(ret[nt], fs[token])
+                    # if the token is a nonterm
+                    if token in self.nonterms():
+                        # the follow set of that nonterm includes tmp
+                        updated |= update(ret[token], tmp)
+                    # if the token can produce epsilon
+                    if token in epsilons:
+                        # tmp should include this token's first set, minus epsilon
+                        tmp |= fs[token] - {"#"}
+                    else:
+                        # forget about what we had earlier; tmp is now the first set of the current token
+                        tmp = fs[token]
+            # if the set didn't change
             if not updated:
+                # stop
                 return ret
 
-    def lex(self, ip: Iterable[str], spcl: Dict[str, Pattern[str]] = None) -> Sequence[Tuple[str, str]]:
+    """
+    Produces a sequence of terminals out of a raw string.
+    
+    :param ip: An iterable of lines to lex.
+    :param spcl: Special rules to match.
+    These will substitute given regexes for tokens in the CFG instead of matching the text literally.
+    For example:
+    {
+        "ID": "[a-zA-Z]+"
+        "NUM": "\\d+"
+    }
+    
+    :returns: [(raw token, terminal in cfg)...]
+    """
+    def lex(self, ip: Iterable[str], spcl: Dict[str, str] = None) -> Sequence[Tuple[str, str]]:
+        # cannot have mutable default arguments
         if spcl is None:
             spcl = {}
+
+        # for each string, produce a regex
+        spcl = {x: re.compile(spcl[x]) for x in spcl}
+
+        # produce a list of tokens to check, exclude any specials as we check those later
+        terms = set(self.terminals()) - set(spcl)
 
         ret: List[Tuple[str, str]] = []
         for line in ip:
             line = line.strip()
+            # while there are stil tokens to be read
             while line != "":
                 longest: Tuple[str, str] = ("", "")
-                for s in self.terminals():
+                # for each terminal, check if the line starts with that terminal
+                for s in terms:
                     if line.startswith(s):
+                        # if so, set longest to the longer of the two tokens
                         longest = (s, s) if len(s) > len(longest[0]) else longest
+                # now check specials
                 for name in spcl:
+                    # match the beginning of the string with the regex
                     r = spcl[name].match(line, 0)
                     if r is not None:
+                        # set the longer of the two tokens
                         longest = (r[0], name) if len(r[0]) > len(longest[0]) else longest
+                # if we matched nothing, throw
                 if longest[0] == "":
                     raise CFGException("Invalid token starting with \"" + line + "\"")
+                # save the token we read
                 ret.append(longest)
+                # now get rid of the token we just read from the line
                 line = line[len(longest[0]):].strip()
         return ret
 
+    """
+    Returns true if a non-terminal is in the grammar.
+    """
     def __contains__(self, nt: str) -> bool:
         return nt in self.__rules
 
+    """
+    Gets the rules for a given non-terminal.
+    """
     def __getitem__(self, nt: str) -> Nonterm:
         return self.__rules[nt]
 
+    """
+    Iterates producing (nonterm, rule) for each rule in each nonterm.
+    """
     def __iter__(self) -> Iterator[Tuple[str, Tuple[str]]]:
         for nt in self.nonterms():
             for prod in self[nt]:
                 yield (nt, prod)
 
+    """
+    Returns the amount of nonterms in the grammar.
+    """
     def __len__(self) -> int:
         return len(self.__rules)
 
+    """
+    Converts the grammar to a string.
+    The first rule is always the start symbol.
+    """
     def __str__(self) -> str:
         return reduce(lambda a, v: a + "\n" + str(v), [self[nt] for nt in self.nonterms()], "")[1:]
+
+
+class ItemSet:
+    __nt: str
+    __prod: Tuple[str]
+    __follow: Set[str]
+    __dotpos: int
+
+    def __init__(self, nt: str, prod: Tuple[str], follow: Set[str], dotpos: int):
+        self.__nt = nt
+        self.__prod = prod
+        self.__follow = follow
+        self.__dotpos = dotpos
+
+    def __str__(self):
+        return self.__nt + " -> " + " ".join(self.__prod[0:self.__dotpos]) + " . " + " ".join(self.__prod[self.__dotpos:])
+
+
+class LR1Parser:
+    __itemsets: List[Tuple[Tuple[str], str]]
+
+    def __init__(self, g: Grammar):
+
 
 
 def main():
@@ -380,8 +490,8 @@ def main():
         "   return 0;",
         "}"
     ], {
-        "ID": re.compile("[A-Za-z]+"),
-        "NUM": re.compile("\\d+|\\d\\.\\d+")
+        "ID": "[A-Za-z]+",
+        "NUM": "\\d+|\\d\\.\\d+"
     }))
     print()
     print(str(x))
